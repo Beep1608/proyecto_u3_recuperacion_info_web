a6bdcab61de325864d8b95ff0aa781d0b6f78829
Fidel Zarco <fidel.zarcoa@sfa.michoacan.gob.mx>
2025-04-25 16:38:56 -0600

    Identificacion de entidades

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import PyPDF2
 import os
+import spacy
 class Nlp:
     filename = None
     lector = None
@@ -8,19 +9,21 @@ class Nlp:
         self.filename = input("Ingrese el nombre del archivo PDF: ")
 
 
-    def run(self):
-        self.output_dir = self.make__dirs(self.filename)
+    def run(self, extraccion_info):
+        print("file name: ", self.filename)
+        self.output_dir = self.make__dirs(self)
         self.lector = self.get_lector_pdf(self.filename)
-        self.save_pages(self.lector, 5, self.output_dir)
-        self.close_pdf(self.lector)
+        self.save_pages(self.lector, 15, self.output_dir)
+  
+        extraccion_info.mostrar_entidades(self.read_pages(self.lector, 15))
 
-    def make__dirs(name_file):
-        base_name = name_file.replace(".pdf", "")
+    def make__dirs(self, filename):
+        base_name = self.filename.replace(".pdf", "")
         output_dir = os.path.join(base_name, "pages")
         os.makedirs(output_dir, exist_ok=True)
         return output_dir
 
-    def get_lector_pdf(filename):
+    def get_lector_pdf(self, filename):
         if not os.path.exists(filename):
             print(f"El archivo '{filename}' no existe.")
             return None
@@ -28,12 +31,12 @@ class Nlp:
         lector = PyPDF2.PdfReader(pdf)
 
         return lector
-    def close_pdf(pdf):
-        pdf.close()
+    
+
 
 
     # Verificar que el numbero de paginas no sea mayor al total de paginas del pdf
-    def save_pages (lector, number_pages, output_dir):
+    def save_pages (self,lector, number_pages, output_dir):
     
         n = len(lector.pages)
         if(n>number_pages):
@@ -48,6 +51,45 @@ class Nlp:
                     archivo.write(texto)
                     pages_created += 1
         print(f"Se han creado {pages_created} archivos de texto en '{output_dir}'.")
+    
+    def read_pages (self,lector, number_pages):
+        texto = ""
+        n = len(lector.pages)
+        if(n>number_pages):
+            n = number_pages
+        pages_created = 0
+        for x in range(n):
+            pagina = lector.pages[x]
+            texto = pagina.extract_text()
+            if texto and texto.strip():  
+                print(f"Texto de la página {x}:\n{texto}\n")
+                pages_created += 1
+        print(f"Se han leído {pages_created} páginas del PDF.")
+        return texto
+    
+
+    class ExtraccionInformacion:
+        model = None
+        tipos_validos = {'personas': 'PER', 'lugares': 'LOC', 'fechas': 'DATE', 'organizaciones': 'ORG'}
+   
+        def __init__(self, model):
+            self.model = spacy.load(model)
+
+        def mostrar_entidades(self, texto):
+            doc = self.model(texto)
+            for entidad in doc.ents:
+                print(f"Texto: {entidad.text}, Tipo: {entidad.label_}")
+
+        def responder_pregunta(self, texto, pregunta):
+            doc_pregunta = self.model(pregunta)
+            palabras_clave = [entidad.text.lower() for entidad in doc_pregunta.ents ]
+            respuesta = "No puedo responder a esa pregunta."
+            #Agregar codigo faltante despues de esta linea 
+          
+               
+                    
+       
+
 
 #Main
-Nlp().run()
+Nlp().run(Nlp.ExtraccionInformacion("en_core_web_sm"))
